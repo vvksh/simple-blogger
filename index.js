20dff7511d2d4752ae2385c14e1464fced3e90e5
Vivek Sah <vivek.sah32+github@gmail.com>
2020-05-10 15:40:35 -0700

    Add project description, social links in index.html , home redirect links, github repo links in post.html and use whitey.css instead of air and modest

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-
+var githubUserName = 'vksah32'
+var linkedinUserName = 'vksah'
+var twitterUserName = 'viveksyz'
 
 function fetchInfo (numProjects) {
     console.log("fetching")
+    addAbout() 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
          if (this.readyState == 4 && this.status == 200) {
@@ -10,22 +13,34 @@ function fetchInfo (numProjects) {
             
          }
     };
-    xhttp.open("GET", "https://api.github.com/users/vksah32/repos?per_page="+numProjects+"&sort=updated", true);
+
+    xhttp.open("GET", `https://api.github.com/users/${githubUserName}/repos?per_page=${numProjects}&sort=updated`, true);
     xhttp.setRequestHeader("Accept", "application/vnd.github.inertia-preview+json");
     xhttp.send(null);
 }
 
-https://github.com/vksah32/mapreduce/blob/master/README.md
+function addAbout() {
+    var aboutHtml = `
+            <a class="fa fa-github" href="https://github.com/${githubUserName}"> </a> / 
+            <a class="fa fa-linkedin" href="https://www.linkedin.com/in/${linkedinUserName}/"> </a> /
+            <a class="fa fa-twitter" href="https://www.twitter.com/${twitterUserName}/"> </a>
+        `;
+    document.getElementById("about").innerHTML = aboutHtml;
+}
 
 function handleGithubResponse(arr) {
     console.log(arr)
     var out = "";
     var i;
     for(i = 0; i < arr.length; i++) {
-        out +=  '<a class="link" href="/post.html?repoName='+arr[i].name + '">'+ arr[i].name + '</a><br>';
-
-
-        // out +=  '<div class="link" onclick="redirect(\''+ arr[i].name +'\')">' + arr[i].name + '</div><br>';
+        // don't add forked repos
+        if (!arr[i].fork) {
+            var description = ''
+            if (arr[i].description != null) {
+                description = arr[i].description
+            }
+            out +=  `<div style="margin-top:10px"> <a  href="/post.html?repoName=${arr[i].name}">${arr[i].name} </a><i style="opacity: 0.6"> ${description}</i> </div>`;
+        }
     }
     document.getElementById("content").innerHTML = out;
 }
@@ -39,6 +54,10 @@ function getPost() {
     console.log("getting post")
     const urlParams = new URLSearchParams(window.location.search);
     var repoName = urlParams.get('repoName')
+    var githubLink = `https://github.com/${githubUserName}/${repoName}`
+    var homeLink = 'index.html'
+    document.getElementById("home-link").innerHTML = `<a class="fa fa-home" href="${homeLink}"> Home </a>`
+    document.getElementById("github-link").innerHTML = `<a class="fa fa-github" href="${githubLink}"> See code</a>`
 
     var readMeUrl = 'https://raw.githubusercontent.com/vksah32/'+ repoName + '/master/README.md'
     var xhttp = new XMLHttpRequest();
@@ -54,6 +73,7 @@ function getPost() {
 
 function renderMD(text) {
     var converter =  new showdown.Converter();
+    
     document.getElementById("post-content").innerHTML = converter.makeHtml(text);
 
 }
